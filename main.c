30d5248d8cc75240aa28738440bcf46eda4811a6
G-Aswin <aswin.g.rns@gmail.com>
2021-06-15 18:42:23 +0530

    hobby feature added

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ struct people
     char gender;
     int age;
     char city[100];
+    char hobby[25];
 
     int friends;
 };
@@ -24,36 +25,30 @@ int find_user_index(struct people data[], int accounts, char *username);
 void display_connections(int matrix[][100], struct people data[], int accounts, user x);
 int isNotFollowingAnyone(user x, int matrix[][100], int accounts);
 void bfs_with_distance(int matrix[][100], struct people data[], int accounts, user x, int limit);
-void preloadData(int adjacency_matrix[][100], struct people database[], int *accounts);
+void preload(int adjacency_matrix[][100], struct people database[], int *accounts);
+void recommend_friends_wrt_hobby(int matrix[][100], struct people data[], int accounts, user x, int limit);
+void friends_sharing_hobby(int matrix[][100], struct people data[], int accounts, user x);
+void display_menu_options();
 
+
+//Main function
 int main(int argc, char *argv[])
 {
     struct people database[100];                        //array of struct which actually holds user data
-    int adjacency_matrix[100][100] = {0};     //adjacency matrix storing the edge data
-    int active_accounts = 0;       // number of active accounts
+    int adjacency_matrix[100][100] = {0};               //adjacency matrix storing the edge data
+    int active_accounts = 0;                            // number of active accounts
 
     char name1[100], name2[100];  
 
 
     if (argc > 1)
-        preloadData(adjacency_matrix, database, &active_accounts);
+        preload(adjacency_matrix, database, &active_accounts);
 
 
     int ch, n, idx1, idx2, lim;
     while (1)
     {
-        printf("\n***USER MENU***\n");
-        printf("\n1. Create account(s)");
-        printf("\n2. Add a new connection/friend");
-        printf("\n3. Remove a connection/friend");
-        printf("\n4. Display all friends");
-        printf("\n5. **Recommend Friends**");
-
-        printf("\n\n***Master MENU***\n");
-        printf("\n11. Display All Data");
-        printf("\n22. Display Matrix");
-
-        printf("\n\n: ");
+        display_menu_options();
         scanf("%d", &ch);
 
         switch(ch)
@@ -228,6 +223,84 @@ int main(int argc, char *argv[])
                     }
                     break;
 
+            case 6: printf("\nHow many friends should be displayed maximum? : ");
+                    scanf("%d", &lim);
+
+                    printf("\nAccount choices by name(1) or id(2)? Enter choice : ");
+                    scanf("%d", &n);
+
+                    if (n == 1)
+                    {
+                        printf("\nEnter username : ");
+                        scanf(" %[^\n]s", name1);                        
+                        printf("\nEnter username : ");
+
+                        idx1 = find_user_index(database, active_accounts, name1);
+                        if (idx1 == -1)
+                        {
+                            printf("\nUsername not founf!");
+                            break;
+                        }
+
+                        recommend_friends_wrt_hobby(adjacency_matrix, database, active_accounts, &database[idx1], lim);
+                    }
+                    else if (n == 2)
+                    {
+                        printf("\nEnter user ID : ");
+                        scanf("%d", &idx1);
+
+                        if (idx1 >= active_accounts)
+                        {
+                            printf("\nInvalid ID");
+                            break;
+                        }
+
+                        recommend_friends_wrt_hobby(adjacency_matrix, database, active_accounts, &database[idx1], lim);
+                    }
+                    else
+                    {
+                        printf("\nInvalid choice.");
+                        break;
+                    }
+                    break;
+
+            case 7: printf("\nAccount choices by name(1) or id(2)? Enter choice : ");
+                    scanf("%d", &n);
+
+                    if (n == 1)
+                    {
+                        printf("\nEnter username : ");
+                        scanf("%s", name1);
+
+                        idx1 = find_user_index(database, active_accounts, name1);
+                        if (idx1 == -1)
+                        {
+                            printf("\nUsername not found!");
+                            break;
+                        }
+
+                        friends_sharing_hobby(adjacency_matrix, database, active_accounts, &database[idx1]);
+                    }
+                    else if (n == 2)
+                    {
+                        printf("\nEnter user ID : ");
+                        scanf("%d", &idx1);
+
+                        if (idx1 >= active_accounts)
+                        {
+                            printf("\nInvalid ID");
+                            break;
+                        }
+
+                        friends_sharing_hobby(adjacency_matrix, database, active_accounts, &database[idx1]);
+                    }
+                    else
+                    {
+                        printf("\nInvalid choice.");
+                        break;
+                    }
+                    break;
+
             case 11:    for (int i = 0; i < active_accounts; i++)
                         {
                             display(&database[i]);
@@ -261,6 +334,8 @@ void accept(user P)
     scanf("%d", &P->age);
     printf("City: ");
     scanf("%s", P->city);
+    printf("Hobby: ");
+    scanf("%s", P->hobby);
     P->friends = 0;
 }
 
@@ -272,6 +347,7 @@ void display(user P)
 	printf("Age : %d\n",P->age);
 	printf("City : %s\n",P->city);
     printf("Friend count : %d\n", P->friends);
+    printf("Hobby : %s\n", P->hobby);
 }
 
 
@@ -351,6 +427,29 @@ void display_connections(int matrix[][100], struct people data[], int accounts, 
     }
 }
 
+
+void friends_sharing_hobby(int matrix[][100], struct people data[], int accounts, user x)
+{
+    //print all the names of connections of a user x
+
+    if (isNotFollowingAnyone(x, matrix, accounts))
+    {
+        printf("\nNo Friends Added!");
+        return;
+    }
+
+    printf("\nFriends of %s who also like %s : ", x->name, x->hobby);
+    for (int i = 0, j = 1; i < accounts; i++)
+    {
+        if (matrix[x->id][i] == 1 && strcmp(x->hobby, data[i].hobby) == 0)
+            printf("\n%d. %s", j++, data[i].name);
+    }
+}
+
+
+
+
+
 void bfs_with_distance(int matrix[][100], struct people data[], int accounts, user x, int limit)
 {
     // limit is the max number of suggestions that the user x wants
@@ -361,6 +460,7 @@ void bfs_with_distance(int matrix[][100], struct people data[], int accounts, us
         return;
     }
 
+    // First column is for visited, second column is for distance
     int visited[100][2] = {0};
     int queue[200], front = 0, rear = -1;
 
@@ -391,10 +491,52 @@ void bfs_with_distance(int matrix[][100], struct people data[], int accounts, us
             }
         }
     }
-
 }
 
-void preloadData(int adjacency_matrix[][100], struct people database[], int *active_accounts)
+void recommend_friends_wrt_hobby(int matrix[][100], struct people data[], int accounts, user x, int limit)
+{
+    // limit is the max number of suggestions that the user x wants
+    // print the suggested persons name, age, gender and bfs distance from user x
+    if (isNotFollowingAnyone(x, matrix, accounts))
+    {
+        printf("\nYou are not following anyone! Unable to produce suggestions.");
+        return;
+    }
+
+    // First column is for visited, second column is for distance
+    int visited[100][2] = {0};
+    int queue[200], front = 0, rear = -1;
+
+    queue[++rear] = x->id;
+    visited[x->id][0] = 1;
+    visited[x->id][1] = 0;
+
+    printf("\nPeople with similar hobbies in your network : ");
+
+    int popped;
+    while (front <= rear && limit != 0)
+    {
+        popped = queue[front++];
+
+        for (int i = 0; i < accounts && limit != 0; i++)
+        {
+            if (matrix[popped][i] == 1 && !visited[i][0])
+            {
+                queue[++rear] = i;
+                visited[i][0] = 1;
+                visited[i][1] = visited[popped][1] + 1;
+
+                if (visited[i][1] > 1 && strcmp(data[i].hobby, x->hobby) == 0)
+                {
+                    printf("\n%s(%c), %d yrs old. (%d)", data[i].name, data[i].gender, data[i].age, visited[i][1]);
+                    limit--;
+                }
+            }
+        }
+    }
+}
+
+void preload(int adjacency_matrix[][100], struct people database[], int *active_accounts)
 {
     FILE *fp = fopen("database.txt", "r");
     int count;
@@ -407,6 +549,7 @@ void preloadData(int adjacency_matrix[][100], struct people database[], int *act
         fscanf(fp, " %c", &database[i].gender);
         fscanf(fp, "%d", &database[i].age);
         fscanf(fp, "%s", database[i].city);
+        fscanf(fp, "%s", database[i].hobby);
         database[i].id = i;
     }
     fclose(fp);
@@ -422,4 +565,25 @@ void preloadData(int adjacency_matrix[][100], struct people database[], int *act
         }
     }
     fclose(fp);
+}
+
+void display_menu_options()
+{
+    printf("\n\n");
+    printf("\n***USER MENU***\n");
+    printf("\n1. Create account(s)");
+    printf("\n2. Add a new connection/friend");
+    printf("\n3. Remove a connection/friend");
+    printf("\n4. Display all friends");
+    printf("\n5. **Recommend Friends**");
+    
+    //newly added
+    printf("\n6. **Recommend people with similar hobby**");
+    printf("\n7. **Display my friends with similar hobby**");
+
+    printf("\n\n***Master MENU***\n");
+    printf("\n11. Display All Data");
+    printf("\n22. Display Matrix");
+
+    printf("\n\n: ");
 }
